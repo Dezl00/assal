6376855c4a4b9045bd1f0d16bd1f38379d3df605
Dezl00 <dezl57740@gmail.com>
2026-08-08 13:20:13 +0300

    Remove test files causing build error

--- src/app/api/debug/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { db } from "@/lib/db";
-
-export async function GET() {
-  const article = await db.article.findFirst({
-    orderBy: { createdAt: 'desc' }
-  });
-  return NextResponse.json({ content: article?.content });
-}
--- src/app/test-editor/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import ReactQuill from "react-quill-new";
-import "react-quill-new/dist/quill.snow.css";
-export default function Test() {
-  return <ReactQuill value={'<a href="https://www.youtube.com/embed/grFQygwA27k?showinfo=0">https://www.youtube.com/embed/grFQygwA27k?showinfo=0</a>'} />
-}
